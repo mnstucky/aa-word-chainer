9d019e739aee40faa0b8e9671f345906b00b76b9
Matt Stucky <mnstucky@gmail.com>
2020-03-22 17:17:32 -0500

    Add dictionary reader

--- wordchainer.rb
+++ wordchainer.rb
@@ -1,4 +1,14 @@
 class WordChainer
 
+    attr_reader :dictionary
+    
+    def initialize
+        @dictionary = File.read("dictionary.txt").split("\n")
+    end
 
 end
+
+# code for testing
+
+w = WordChainer.new
+p w.dictionary
